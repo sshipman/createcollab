34e8d4de1fa23b2daf22eb6180bb7fd292ea32f5
Scott K. Fraley <scott_fraley@hotmail.com>
2025-02-01 17:01:58 -0800

    got a quick map up on the home screen.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:createcollab/components/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:map_launcher/map_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,9 +27,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: const Center(
-        child: Text("Home Page"),
-      ),
+      body: Center(
+        //child: Text("Home Page"),
+        child: OutlinedButton(
+          onPressed: () async {
+            final availableMaps = await MapLauncher.installedMaps;
+            print(availableMaps);
+
+            await availableMaps.first.showMarker(
+              coords: Coords(37.759392, -122.5107336),
+              title: "Ocean Beach",
+            );
+          },
+          child: Text('Show on Map'),
+      )),
     );
   }
 }
